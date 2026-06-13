3a44b50656a21645bb470e31efd194a96afc32cb
Castorice <Special-Texas@outlook.com>
2026-06-13 19:49:57 +0800

    Add files via upload

---
+++ src/main/java/com/castorice/gregtech_machines_addon/Config.java
@@ -0,0 +1,8 @@
+package com.castorice.gregtech_machines_addon;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = GregTechMachinesAddon.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class Config {
+
+}
---
+++ src/main/java/com/castorice/gregtech_machines_addon/GregTechMachinesAddon.java
@@ -0,0 +1,9 @@
+package com.castorice.gregtech_machines_addon;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(GregTechMachinesAddon.MODID)
+public class GregTechMachinesAddon {
+    public static final String MODID = "gregtech_machines_addon";
+
+}
